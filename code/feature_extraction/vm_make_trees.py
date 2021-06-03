e06f2e77bb8c9095da1d8291065d01c61406edb7
Nicole Macher <s2096077@ed.ac.uk>
2021-06-03 20:14:36 +0100

    produces outputs in splits

--- code/feature_extraction/vm_make_trees.py
+++ code/feature_extraction/vm_make_trees.py
@@ -60,6 +60,48 @@ def do_section(out_dir_trees, out_dir_ids, name):
                                 clean_entry = entry
                             tree.append(clean_entry)
 
+def split(out_dir_trees, out_dir_ids):
+    test_sentence_ids = ["cd6_00_"+str(number) for number in range(1, 1251)]
+    print(test_sentence_ids)
+    dev_sentence_ids = ["cd8_00_" + str(number) for number in range(1, 1251)]
+
+    test_trees_file =os.path.join(out_dir_trees, '%s.trees' % "test")
+    test_trees = open(test_trees_file, 'w')
+    train_trees_file =os.path.join(out_dir_trees, '%s.trees' % "train")
+    train_trees = open(train_trees_file, 'w')
+    dev_trees_file =os.path.join(out_dir_trees, '%s.trees' % "dev")
+    dev_trees = open(dev_trees_file, 'w')
+
+    test_sentence_id_file =os.path.join(out_dir_ids, '%s_sent_ids.txt' % "test")
+    test_sentence_id_file_open = open(test_sentence_id_file, 'w')
+    train_sentence_id_file =os.path.join(out_dir_ids, '%s_sent_ids.txt' % "train")
+    train_sentence_id_file_open = open(train_sentence_id_file, 'w')
+    dev_sentence_id_file =os.path.join(out_dir_ids, '%s_sent_ids.txt' % "dev")
+    dev_sentence_id_file_open = open(dev_sentence_id_file, 'w')
+
+    with open(os.path.join(out_dir_ids, 'all_sent_ids.txt')) as sentence_id_file:
+        with open(os.path.join(out_dir_trees,
+                               'all.trees')) as trees_file:
+            all_sentence_ids = sentence_id_file.readlines()
+            all_trees = trees_file.readlines()
+            for i, sentence_id in enumerate(all_sentence_ids):
+                if sentence_id.strip() in test_sentence_ids:
+                    print("test")
+                    test_trees.write(all_trees[i])
+                    test_sentence_id_file_open.write(sentence_id)
+                elif sentence_id.strip() in dev_sentence_ids:
+                    dev_trees.write(all_trees[i])
+                    dev_sentence_id_file_open.write(sentence_id)
+                else:
+                    train_trees.write(all_trees[i])
+                    train_sentence_id_file_open.write(sentence_id)
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     path_to_vm_penn_files = "/afs/inf.ed.ac.uk/user/s20/s2096077/prosody_nlp/verbmobil/verbmobil_treebank/eng/penn_files/penn_files"
@@ -69,3 +111,4 @@ if __name__ == '__main__':
     out_dir_ids = '/afs/inf.ed.ac.uk/user/s20/s2096077/prosody_nlp/data/vm/input_features/'
     # TODO: Add something to specify train, test, and dev
     do_section(out_dir_trees, out_dir_ids, "all")
+    split(out_dir_trees, out_dir_ids)
