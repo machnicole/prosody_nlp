ea6b109b07ca004f44360abf76c2267d05fab7d7
Nicole Macher <s2096077@ed.ac.uk>
2021-06-03 18:06:27 +0100

    don't remove anything

--- code/self_attn_speech_parser/src/main_sparser.py
+++ code/self_attn_speech_parser/src/main_sparser.py
@@ -197,13 +197,15 @@ def run_train(args, hparams):
     to_keep = to_keep.union(wsj_sents)
     if not hparams.seg:
         train_parse = [tree.convert() for tree in train_treebank]
-        
+
+    # comment this back in, when using speech features:
     # Removing sentences without speech info
-    to_remove = set(train_sent_ids).difference(to_keep)
-    to_remove = sorted([train_sent_ids.index(i) for i in to_remove])
-    for x in to_remove[::-1]:
-        train_parse.pop(x)
-        train_sent_ids.pop(x)
+    # to_remove = set(train_sent_ids).difference(to_keep)
+    # to_remove = sorted([train_sent_ids.index(i) for i in to_remove])
+    # for x in to_remove[::-1]:
+    #     train_parse.pop(x)
+    #     train_sent_ids.pop(x)
+
     train_set = list(zip(train_sent_ids, train_parse)) # EKN train_set is a list of tuples: (sent_id, tree)
     print("Loaded {:,} training examples.".format(len(train_set)))
 
@@ -220,13 +222,15 @@ def run_train(args, hparams):
         
     dev_pause_path = os.path.join(args.feature_path, args.prefix + \
             'dev_pause.pickle')
-    with open(dev_pause_path, 'rb') as f:
-        dev_pause_data = pickle.load(f, encoding='latin1')
-    to_remove = set(dev_sent_ids).difference(set(dev_pause_data.keys()))
-    to_remove = sorted([dev_sent_ids.index(i) for i in to_remove])
-    for x in to_remove[::-1]:
-        dev_treebank.pop(x)
-        dev_sent_ids.pop(x)
+    # comment this back in, when using speech features:
+    # with open(dev_pause_path, 'rb') as f:
+    #     dev_pause_data = pickle.load(f, encoding='latin1')
+    # to_remove = set(dev_sent_ids).difference(set(dev_pause_data.keys()))
+    # to_remove = sorted([dev_sent_ids.index(i) for i in to_remove])
+    # for x in to_remove[::-1]:
+    #     dev_treebank.pop(x)
+    #     dev_sent_ids.pop(x)
+
     #if hparams.max_len_dev > 0:
     #    dev_treebank = [tree for tree in dev_treebank if \
     #            len(list(tree.leaves())) <= hparams.max_len_dev]
