2c87487fc40b21d02fcbd63e39d9ddf847b95712
machnicole <53852211+machnicole@users.noreply.github.com>
2021-06-07 20:02:38 +0200

    Update predicting_vm_eng_job.sh

--- code/self_attn_speech_parser/predicting_vm_eng_job.sh
+++ code/self_attn_speech_parser/predicting_vm_eng_job.sh
@@ -121,8 +121,8 @@ mkdir -p ${SCRATCH_HOME}/prosody_nlp/code/self_attn_speech_parser/results
 rsync --archive --update --compress --progress ${src_path}/ ${dest_path}
 
 #This can take very long time
-dest_path2=${SCRATCH_HOME}/prosody_nlp/code/self_attn_speech_parser/models
-src_path2=${repo_home}/code/self_attn_speech_parser/models
+dest_path2=${SCRATCH_HOME}/prosody_nlp/code/self_attn_speech_parser/models/vm_eng_no_speech_model_dev=93.09.pt
+src_path2=${repo_home}/code/self_attn_speech_parser/models/vm_eng_no_speech_model_dev=93.09.pt
 rsync --archive --update --compress --progress ${src_path2}/ ${dest_path2}
 
 # ==============================
