73f016ec6715655c1def538822c4d3086aed7e00
machnicole <53852211+machnicole@users.noreply.github.com>
2021-06-21 11:26:43 +0200

    Update turn_predicting_vm_eng_job_sp.sh

--- code/self_attn_speech_parser/cluster_scripts/turn_predicting_vm_eng_job_sp.sh
+++ code/self_attn_speech_parser/cluster_scripts/turn_predicting_vm_eng_job_sp.sh
@@ -123,8 +123,8 @@ mkdir -p ${SCRATCH_HOME}/prosody_nlp/code/self_attn_speech_parser/results
 rsync -r --archive --update --compress --progress ${src_path}/ ${dest_path}
 
 #This can take very long time
-dest_path2=${SCRATCH_HOME}/prosody_nlp/code/self_attn_speech_parser/models/turn_vm_eng_with_speech_model_dev=94.28.pt
-src_path2=${repo_home}/code/self_attn_speech_parser/models/turn_vm_eng_with_speech_model_dev=94.28.pt
+dest_path2=${SCRATCH_HOME}/prosody_nlp/code/self_attn_speech_parser/models/turn_vm_eng_sp_model_dev=94.35.pt
+src_path2=${repo_home}/code/self_attn_speech_parser/models/turn_vm_eng_sp_model_dev=94.35.pt
 rsync --update --compress --progress ${src_path2} ${dest_path2}
 
 # ==============================
